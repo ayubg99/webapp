b39bd683a843b938a4e421a84899bfdcaf512407
Ayoub <ayubgharbi@gmail.com>
2025-09-03 17:48:09 +0200

    spring mvc

--- src/main/java/com/telesku/simpleWebApp/controller/HomeController.java
+++ src/main/java/com/telesku/simpleWebApp/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.telesku.simpleWebApp;
+package com.telesku.simpleWebApp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
