29dee532904c5d5a2724634925ccb042b0265bbf
Ayoub <ayubgharbi@gmail.com>
2025-09-04 23:37:43 +0200

    Spring Web Http methods GET and POST

--- src/main/java/com/telesku/simpleWebApp/controller/ProductController.java
+++ src/main/java/com/telesku/simpleWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.telesku.simpleWebApp.controller;
 import com.telesku.simpleWebApp.model.Product;
 import com.telesku.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,18 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return service.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        service.addProduct(prod);
     }
 }
