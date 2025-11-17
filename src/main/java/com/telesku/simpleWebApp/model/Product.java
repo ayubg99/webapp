1ed9ee172545510ee761fad6ba7553f45e629116
Ayoub <ayoubg9919@gmail.com>
2025-11-17 15:14:48 +0100

    Spring Data JPA with JpaRepository

--- src/main/java/com/telesku/simpleWebApp/model/Product.java
+++ src/main/java/com/telesku/simpleWebApp/model/Product.java
@@ -1,12 +1,16 @@
 package com.telesku.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
+@Entity
 public class Product {
 
+    @Id
     private int prodId;
     private String prodName;
     private int price;
